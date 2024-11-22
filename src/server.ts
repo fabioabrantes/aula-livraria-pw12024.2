c852b40dfddc37c9afe9e2a56c8ed6e5d7302089
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2024-11-22 16:14:28 -0300

    aula 22/11/2024

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import {v4 as geraNumeroAleatorio} from 'uuid';
+
 type Book = {
   id: string;
   autor: string;
@@ -12,35 +14,54 @@ type User = {
   books: Book[];
 }
 
-const clients: User[] = [];
+let clients: User[] = [];
 
 const server = express();
 server.use(express.json());
+//lista todos os usuarios
 server.get('/users', (request, response) => {
-  const nameHost = request.hostname;
-  response.status(200).send(nameHost);
+ return response.status(200).json(clients);
 });
+server.get('/users/:id', (request, response) => {
+  const id = request.params.id;
+  const user = clients.find(client => client.id === id);
+  if(!user){
+    return response.status(400).json("error:usuário inexistente");
+  }
+  return response.status(200).json(user);
+})
 server.post('/users', (request, response) => {
   const dados = request.body as User;
-  const user:User = {
-    id: dados.id,
-    cpf:dados.cpf,
-    name:dados.name,
-    books:[]
+  //validar numeros
+  const user: User | null = {
+    id: geraNumeroAleatorio(),
+    cpf: dados.cpf,
+    name: dados.name,
+    books: []
   }
-  if(!user){
-    response.status(400).json("error:usaario nao cadastrado")
+  if (!user) {
+    return response.status(400).json("error:usaario nao cadastrado")
   }
-  response.status(200).json(user);
+  clients.push(user);
+  return response.status(201).json({message:"cadastro realizado com sucesso"});
+});
+server.delete('/users/:id', (request, response) => {
+  const id = request.params.id;
+  const updatedUsers = clients.filter(client => client.id !== id);
+  if(updatedUsers.length === clients.length){
+    return response.status(400).json("error:usuário inexistente.");
+  }
+  clients = [...updatedUsers];
+  return response.status(400).json({message:"usuario removido"});
+});
+server.put('/users/:id', (request, response) => {
+  const nome = request.params.name;
+  const corpo = request.body;
+  return response.status(200).json({ nome, corpo });
 });
 
-server.delete('/users', (request, response) => {
- 
-});
-server.put('/users', (request, response) => {
- 
-});
+// CRUD DA TABELA BOOKS
 
 server.listen("3000", () =>
-  console.log("server online on port 3001")
+  console.log("server online on port 3000")
 );
